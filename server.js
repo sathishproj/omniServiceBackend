8ed7de90b915c359785e1bb0ecd3f2f714f37a5e
nani <pallesathish4044@gmail.com>
2025-06-04 14:44:10 +0530

    url changes

--- server.js
+++ server.js
@@ -19,24 +19,26 @@ app.use(express.json());
 
 app.use(cors());
 app.use(bodyParser.json()) // this will return a valid express middleware for parsing Json data.
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","*");
-    res.setHeader("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept");
-    res.setHeader("Access-Control-Allow-Methods","GET,POST,PATCH,DELETE,OPTIONS")
-    next();
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,OPTIONS")
+  next();
 })
 
 app.use(cors({
-  origin:"http://localhost:4200",
+  // origin:"http://localhost:4200",
+  origin:"https://omni-project-gamma.vercel.app",
+  // origin: ['http://localhost:4200', 'https://omni-project-gamma.vercel.app'],
   credentials: true,
 }));
 
 app.use("/", userRouter);
 app.use("/", specialtiesRouter);
 app.use("/", doctorRoutes);
-app.use("/",mailRoutes);
+app.use("/", mailRoutes);
 
 // Sample route to hit LeadSquared API
 // app.post('/submit', async (req, res) => {
@@ -45,8 +47,8 @@ app.use("/",mailRoutes);
 // Connect to Database & Start Server
 const startServer = async () => {
   try {
-    // await connectDB();
-    // console.log("✅ Database successfully connected");
+    await connectDB();
+    console.log("✅ Database successfully connected");
 
     app.listen(3000, () => {
       console.log("🚀 Server is running on http://localhost:3000");
